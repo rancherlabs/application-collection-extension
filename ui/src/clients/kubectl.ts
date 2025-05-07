7cc1a9b4adc9a743e5cd487f7d1927736b5723e6
Pablo Caraballo Llorente <pablo.caraballo@suse.com>
2025-05-07 12:40:14 +0200

    Add selector for kubernetes context in the settings page

--- ui/src/clients/kubectl.ts
+++ ui/src/clients/kubectl.ts
@@ -59,3 +59,36 @@ export async function getServices(ddClient: DockerDesktopClient, selectors: { ke
       })
   })
 }
+
+export async function getContexts(ddClient: DockerDesktopClient): Promise<{ name: string, selected?: boolean }[]> {
+  return new Promise<{ name: string, selected?: boolean }[]>((resolve, reject) => {
+    ddClient.extension.host?.cli.exec('kubectl', [
+      'config', 'current-context'
+    ])
+      .then(currentContextResult => {
+        const selectedContext: string = currentContextResult.stdout.replace('\n', '')
+        ddClient.extension.host?.cli.exec('kubectl', [
+          'config', 'get-contexts',
+          '-o', 'name'
+        ])
+          .then(contextsResult => {
+            const contexts: string[] = contextsResult?.stdout.split('\n')
+            resolve(contexts.slice(0, contexts.length - 1).map(name => { return { name, selected: name == selectedContext } }))
+          })
+          .catch((e) => {
+            console.error('Unexpected error getting k8s contexts', e)
+            reject('Unexpected error reading kubernetes contexts')
+          })
+      })
+      .catch((e) => {
+        console.error('Unexpected error getting current k8s context', e)
+        reject('Unexpected error reading current kubernetes context')
+      })
+  })
+}
+
+export async function useContext(ddClient: DockerDesktopClient, context: string): Promise<void> {
+  await ddClient.extension.host?.cli.exec('kubectl', [
+    'config', 'use-context', context
+  ])
+}
