3cfec7052793ec66c092ca70024fe10471310de9
Pablo Caraballo Llorente <pablo.caraballo@suse.com>
2025-06-20 11:11:37 +0100

    Improve app details UX (#27)

---
+++ ui/src/pages/ApplicationDetailsPage/components/BranchesList/HintIcon.tsx
@@ -0,0 +1,28 @@
+import { SvgIconComponent } from '@mui/icons-material'
+import { PopperProps, Tooltip, Stack, SvgIcon, Typography, Skeleton } from '@mui/material'
+
+export default function HintIcon({ tooltip, icon, text }: { tooltip: string, icon: SvgIconComponent, text: string }) {
+  const popperProps: Partial<PopperProps> = {
+    modifiers: [
+      {
+        name: 'offset',
+        options: {
+          offset: [0, -4]
+        }
+      }
+    ]
+  }
+
+  return (
+    <Tooltip title={ tooltip } PopperProps={ popperProps }>
+      <Stack direction='row' justifyContent='start' alignItems='center' spacing={ 0.5 }>
+        <SvgIcon component={ icon } fontSize='small' sx={ { color: 'text.secondary' } } />
+        <Typography color='text.secondary'>{ text }</Typography>
+      </Stack>
+    </Tooltip>
+  )
+}
+
+export function LoadingHintIcon({ width = 70 }: { width?: number }) {
+  return <Skeleton variant='text' height={ 20 } width={ width }/>
+}
